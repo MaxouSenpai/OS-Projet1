fc6a201b8ef71d68723277e9fa17294ef1e9a487
MaxouSenpai <Maxime.Hauwaert@ulb.ac.be>
2018-11-23 01:42:16 +0100

    Conditions added

--- Test.sh
+++ Test.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 
 run() {
+  if [ ! -e $2 ]
+  then
+    mkdir -p $2
+  fi
+
+  for dir in $1*/
+  do
+    if [ -e $dir ]
+    then
+      dir_name=${dir#$1}
+      author=${dir_name%/}
+      if id $author >/dev/null 2>&1
+      then
+        fileExplorer $dir $2 $3 $4 $author
+      else
+        fileExplorer $dir $2 $3 $4 $USER
+      fi
+    fi
+  done
+}
+
+fileExplorer() {
   for file in $1*
   do
     if [ -d $file ]
@@ -10,10 +32,10 @@ run() {
     elif [ -f $file ]
     then
       filename=${file#$1}
-      user=$(stat -c '%U' $file)
+      #user=$(stat -c '%U' $file)
       name=${filename#*_}
       date=$(date -d @${filename%_*} +"%Y/%m/%d")
-      path=$2$user/$date/
+      path=$2$5/$date/
       mkdir -p $path
       cp $file $path
       mv $path$filename $path$name
@@ -21,8 +43,9 @@ run() {
   done
 }
 
-if [ $# -lt 4 ] ; then
-  echo "Pas assez d'arguments"
+if [ $# -ge 4 ] && [ -d $1 ]
+then
+  run $1 $2 $3 $4
 else
-run $1 $2 $3 $4
+    echo "No"
 fi
