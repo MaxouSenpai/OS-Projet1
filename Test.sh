90e811d54707364d8e41f2ac7be6723440de8797
MaxouSenpai <Maxime.Hauwaert@ulb.ac.be>
2018-11-26 21:52:29 +0100

    Almost finished I guess

--- Test.sh
+++ Test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-run() {
+createPool2() { # $1 Path1 / $2 Path2 / $3 admin / $4 group
   if [ ! -e $2 ]
   then
     mkdir -p $2
   fi
+  setPermissions $2 $3 $4 750
 
   for dir in $1*/
   do
@@ -12,40 +13,53 @@ run() {
     then
       dir_name=${dir#$1}
       author=${dir_name%/}
-      if id $author >/dev/null 2>&1
+      if ! id $author >/dev/null 2>&1
       then
-        fileExplorer $dir $2 $3 $4 $author
-      else
-        fileExplorer $dir $2 $3 $4 $USER
+        author=$(stat -c '%U' $dir)
       fi
+      mkdir -p $2$author
+      setPermissions $2$author $3 $4 750
+      fileExplorer $dir $2 $3 $4 $author
     fi
   done
 }
 
-fileExplorer() {
+fileExplorer() { # $1 Path1 / $2 Path2 / $3 admin / $4 group / $5 author
   for file in $1*
   do
     if [ -d $file ]
     then
-      run $file/ $2 $3 $4
-
+      fileExplorer $file/ $2 $3 $4 $5
     elif [ -f $file ]
     then
       filename=${file#$1}
-      #user=$(stat -c '%U' $file)
       name=${filename#*_}
-      date=$(date -d @${filename%_*} +"%Y/%m/%d")
-      path=$2$5/$date/
+      date=${filename%_*}
+      year=$(date -d @$date +"%Y")
+      month=$(date -d @$date +"%m")
+      day=$(date -d @$date +"%d")
+      path=$2$5/$year/$month/$day/
       mkdir -p $path
+      setPermissions $2$5/$year $3 $4 750
+      setPermissions $2$5/$year/$month $3 $4 750
+      setPermissions $2$5/$year/$month/$day $3 $4 750
       cp $file $path
       mv $path$filename $path$name
+      setPermissions $path$name $author $4 640
     fi
   done
 }
 
+setPermissions() { # $1 file / $2 file_owner / $3 group / $4 octal_permission
+  sudo chown $2 $1
+  sudo chgrp $3 $1
+  sudo chmod $4 $1
+}
+
 if [ $# -ge 4 ] && [ -d $1 ]
 then
-  run $1 $2 $3 $4
+  createPool2 $1 $2 $3 $4
+
 else
-    echo "No"
+    echo "Invalid Arguments"
 fi
